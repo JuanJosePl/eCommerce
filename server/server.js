32c0b870cff2c7769eb743d869fa2b1975011ceb
JuanJosePl <poloj3614@gmail.com>
2024-09-22 15:13:47 -0500

    Actulizando CORS para peticiones desde la url de vercel

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import express from "express";
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import usuarioRoutes from "./routes/userRoutes.js";
@@ -9,49 +9,67 @@ import productRoutes from "./routes/productRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import helmet from 'helmet';
-import { errorHandler } from './utils/errorHandler.js';
-import { apiLimiter } from './middleware/rateLimiter.js';
-import logger from './utils/logger.js';
+import helmet from "helmet";
+import { errorHandler } from "./utils/errorHandler.js";
+import { apiLimiter } from "./middleware/rateLimiter.js";
+import logger from "./utils/logger.js";
 import carritoRoutes from "./routes/cartRoutes.js";
 import cuponRoutes from "./routes/couponRoutes.js";
 import devolucionRoutes from "./routes/returnRoutes.js";
 import notificacionRoutes from "./routes/notificationRoutes.js";
 import wishlistRoutes from "./routes/wishlistRoutes.js";
-import { MONGO_URL, PORT} from "./config/config.js";
+import { MONGO_URL, PORT } from "./config/config.js";
 
 dotenv.config();
 
 const app = express();
 
 // Configuración de Helmet
-app.use(helmet({
-  contentSecurityPolicy: {
-    directives: {
-      defaultSrc: ["'self'"],
-      scriptSrc: ["'self'", "https://trusted.cdn.com"],
-      styleSrc: ["'self'", "https://trusted.cdn.com", "'unsafe-inline'"],
-      imgSrc: ["'self'", "data:", "http://localhost:8000"],
-      connectSrc: ["'self'", "http://localhost:8000"],
-      fontSrc: ["'self'", "https://fonts.gstatic.com"],
-      objectSrc: ["'none'"],
-      frameSrc: ["'none'"],
-    }
-  },
-  crossOriginEmbedderPolicy: false,
-  crossOriginOpenerPolicy: false,
-  crossOriginResourcePolicy: {
-    policy: 'cross-origin'
-  }
-}));
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "https://trusted.cdn.com"],
+        styleSrc: ["'self'", "https://trusted.cdn.com", "'unsafe-inline'"],
+        imgSrc: ["'self'", "data:", "http://localhost:8000"],
+        connectSrc: ["'self'", "http://localhost:8000"],
+        fontSrc: ["'self'", "https://fonts.gstatic.com"],
+        objectSrc: ["'none'"],
+        frameSrc: ["'none'"],
+      },
+    },
+    crossOriginEmbedderPolicy: false,
+    crossOriginOpenerPolicy: false,
+    crossOriginResourcePolicy: {
+      policy: "cross-origin",
+    },
+  })
+);
 
 // Configuración de otros middleware
 app.use(express.json());
-app.use(cors({
-    origin: /^http:\/\/localhost:\d+$/,
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // Permitir el origen local
+      if (/^http:\/\/localhost:\d+$/.test(origin)) {
+        return callback(null, true);
+      }
+      // Permitir la URL de Vercel (frontend)
+      if (
+        origin === "https://e-commerce-git-dev-juanjosepls-projects.vercel.app"
+      ) {
+        return callback(null, true);
+      }
+      // Denegar el acceso para otros orígenes
+      callback(new Error("Not allowed by CORS"));
+    },
     credentials: true,
-    allowedHeaders: ['Authorization', 'Content-Type', 'Accept'],
-}));
+    allowedHeaders: ["Authorization", "Content-Type", "Accept"],
+  })
+);
+
 app.use(cookieParser());
 
 // Configurar el puerto y la URL de MongoDB desde las variables de entorno
@@ -70,22 +88,22 @@ mongoose
   .catch((error) => logger.error(error));
 
 // Ruta raíz
-app.get('/', (req, res) => {
-    res.send('Hola, bienvenido al servidor!');
+app.get("/", (req, res) => {
+  res.send("Hola, bienvenido al servidor!");
 });
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use('/img', express.static(path.join(__dirname, 'public/img')));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/img", express.static(path.join(__dirname, "public/img")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Definir las rutas de la API
 app.use("/api/usuarios", apiLimiter, usuarioRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/productos", apiLimiter, productRoutes);
 app.use("/api/orders", apiLimiter, orderRoutes);
-app.use('/api/cart', carritoRoutes);
+app.use("/api/cart", carritoRoutes);
 app.use("/api/cupons", apiLimiter, cuponRoutes);
 app.use("/api/devoluciones", apiLimiter, devolucionRoutes);
 app.use("/api/notificaciones", apiLimiter, notificacionRoutes);
